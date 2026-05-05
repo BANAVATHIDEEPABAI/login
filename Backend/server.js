01b1e564678295dbe1ace6a029f7335511fcb8bf
BANAVATHIDEEPABAI <bhanu.deepa2006@gmail.com>
2026-05-05 14:35:23 +0000

    ll

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,8 @@ mongoose.connect(process.env.MONGO_URI)
 const authRoutes = require('./routes/auth');
 
 app.use('/api/auth', authRoutes);
-
+app.get('/', (req, res) => {
+  res.send('Welcome! Your app is working 🚀');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
