ef65ea3bc208ca9c9f67e4d6d71e93cb681dc4e7
BANAVATHIDEEPABAI <bhanu.deepa2006@gmail.com>
2026-05-05 14:43:33 +0000

    ll

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth"
+  baseURL: "https://login-8s0t.onrender.com/api/auth"
 });
 
 export default API;
